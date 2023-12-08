a6bc8453652ee2b6d50d4a56b94f21f3459ac26c
Ishitamehra <135868668+Ishitamehra@users.noreply.github.com>
2023-12-08 13:11:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DataGrid from "./components/DataGrid";
+
+const App = () => {
+  return (
+    <div>
+      <h1>This is my React TASK-4</h1>
+      <DataGrid />
+    </div>
+  );
+};
+
+export default App;
